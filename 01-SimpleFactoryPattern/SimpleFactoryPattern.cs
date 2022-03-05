afcdfbbbaae02be57beb0e3cc28a7bf447f83f7c
iwiniwin <iwiniwin@foxmail.com>
2022-03-05 15:41:41 +0800

    添加运行代码

---
+++ 01-SimpleFactoryPattern/SimpleFactoryPattern.cs
@@ -0,0 +1,18 @@
+/*
+简单工厂模式
+
+定义：
+严格意义上来说不是一个真正的模式，但经常被用于封装创建对象的代码
+到底要实例化谁，将来会不会增加实例化的对象，这是很容易变化的部分，应该考虑用一个单独的类来做这个创造实例的过程，这就是工厂
+*/
+using System;
+namespace SimpleFactoryPattern
+{
+    public class Demo 
+    {
+        public static void Execute()
+        {
+            
+        }
+    }
+}
